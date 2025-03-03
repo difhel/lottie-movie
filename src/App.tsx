9d1b4620002e212899d8f2df53ecfc85b5ff6679
Mark Fomin <mark@difhel.dev>
2025-03-03 15:08:12 +0300

    Force dark theme for reels

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Icon28MagicWandOutline, Icon28Video, Icon28ListLikeFill } from '@vkontakte/icons'
 import styles from './App.module.scss'
-import { Tabbar } from '@telegram-apps/telegram-ui'
+import { AppRoot, Tabbar } from '@telegram-apps/telegram-ui'
 import { useState } from 'react'
 import { Films } from './pages/Films/Films'
 import { Loading } from './components/Loading/Loading'
@@ -31,7 +31,7 @@ function App() {
     }
   }
   return (
-    <>
+    <AppRoot appearance={currentTab === TabbarItems.REELS ? 'dark' : undefined}>
       {renderTabContent()}
       <Tabbar>
         <Tabbar.Item text="Фильмы" onClick={() => setCurrentTab(TabbarItems.FILMS)} selected={currentTab === TabbarItems.FILMS}>
@@ -48,7 +48,7 @@ function App() {
           <Icon28Video />
         </Tabbar.Item>
       </Tabbar>
-    </>
+    </AppRoot>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.module.scss'
 import App from './App.tsx'
-import { AppRoot } from '@telegram-apps/telegram-ui';
 
 import '@telegram-apps/telegram-ui/dist/styles.css';
 
 createRoot(document.getElementById('root')!).render(
-  <AppRoot>
-    <App />
-  </AppRoot>
+  <App />
 )
