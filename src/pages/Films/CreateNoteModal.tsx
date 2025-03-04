27564af7104373f6820c4af48f86c84f1cfe2bcc
Mark Fomin <mark@difhel.dev>
2025-03-04 05:37:35 +0300

    Create text note modal

--- src/pages/Films/CreateNoteModal.tsx
+++ src/pages/Films/CreateNoteModal.tsx
@@ -1,6 +1,6 @@
-import { Cell, Input, Modal, Spinner, Tappable, Title } from "@telegram-apps/telegram-ui"
+import { ButtonCell, Cell, Input, Modal, Spinner, Tappable, Textarea, Title } from "@telegram-apps/telegram-ui"
 import { ModalHeader } from "@telegram-apps/telegram-ui/dist/components/Overlays/Modal/components/ModalHeader/ModalHeader"
-import { Icon24CancelCircleOutline, Icon28VideoAddSquareOutline } from "@vkontakte/icons";
+import { Icon24CancelCircleOutline, Icon28Done, Icon28VideoAddSquareOutline } from "@vkontakte/icons";
 import { memo, useCallback, useState } from "react"
 import styles from './Films.module.scss'
 import { SearchSuggestion } from "../../components/SearchSuggestions/SearchSuggestion";
@@ -20,49 +20,107 @@ export const CreateNoteModal = memo<OwnProps>(({ open, onOpenChange, setIsOpen }
     setIsOpen(false);
   }, []);
 
-  return (
-    <Modal
-      open={open}
-      onOpenChange={onOpenChange}
-      header={<ModalHeader />}
-      className={styles.modal}
-    >
-      <Title level="2" weight="1" plain>
-        Создание заметки
-      </Title>
-      <Input
-        status="focused"
-        header="Input"
-        placeholder="Название фильма или заголовок заметки"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-        after={<Tappable Component="div" style={{
-          display: 'flex'
-        }}
-          onClick={() => setValue('')}
-        >
-          <Icon24CancelCircleOutline />
-        </Tappable>} />
+  const [isCreateTextModalOpen, setIsCreateTextModalOpen] = useState(false);
+  const [textNoteName, setTextNoteName] = useState('');
+  const [textNoteDescription, setTextNoteDescription] = useState('');
 
-      <div className={styles.createNoteModalSuggestions}>
-        {!isLoading && (
-          (
-            <Cell
-              before={<Icon28VideoAddSquareOutline />}
-              description="Сохранить заметку о фильме"
-            >
-              Добавить заметку
-            </Cell>
-          )
-        )}
-        {isLoading ? <Spinner size="m" className={styles.spinner} /> : suggestions.map(suggestion => (
-          <SearchSuggestion
-            key={suggestion.id}
-            {...suggestion}
-            onClick={handleClickSuggestion}
-          />
-        ))}
-      </div>
-    </Modal>
+  const handleClickNewText = useCallback(() => {
+    setIsCreateTextModalOpen(true);
+    setTextNoteName(value);
+  }, []);
+
+  const handleSaveTextNote = useCallback(() => {
+    // TODO: create new note
+    setIsCreateTextModalOpen(false);
+    setIsOpen(false);
+  }, []);
+
+  return (
+    <>
+      <Modal
+        open={open}
+        onOpenChange={onOpenChange}
+        header={<ModalHeader />}
+        className={styles.modal}
+        nested
+      >
+        <Title level="2" weight="1" plain>
+          Добавить фильм в список просмотра
+        </Title>
+        <Input
+          status="focused"
+          header="Поиск"
+          placeholder="Название фильма или заголовок заметки"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          after={<Tappable Component="div" style={{
+            display: 'flex'
+          }}
+            onClick={() => setValue('')}
+          >
+            <Icon24CancelCircleOutline />
+          </Tappable>} />
+
+        <div className={styles.createNoteModalSuggestions}>
+          {!isLoading && (
+            (
+              <Cell
+                before={<Icon28VideoAddSquareOutline />}
+                description="Сохранить заметку о фильме"
+                onClick={handleClickNewText}
+              >
+                Добавить заметку
+              </Cell>
+            )
+          )}
+          {isLoading ? <Spinner size="m" className={styles.spinner} /> : suggestions.map(suggestion => (
+            <SearchSuggestion
+              key={suggestion.id}
+              {...suggestion}
+              onClick={handleClickSuggestion}
+            />
+          ))}
+        </div>
+      </Modal>
+
+      <Modal
+        open={isCreateTextModalOpen}
+        onOpenChange={setIsCreateTextModalOpen}
+        header={<ModalHeader />}
+        className={styles.modal}
+        nested
+      >
+        <Title level="2" weight="1" plain>
+          Создание текстовой заметки
+        </Title>
+        <Input
+          status="focused"
+          header="Заголовок"
+          placeholder="Заголовок заметки"
+          value={textNoteName}
+          onChange={e => setTextNoteName(e.target.value)}
+          after={<Tappable Component="div" style={{
+            display: 'flex'
+          }}
+            onClick={() => setTextNoteName('')}
+          >
+            <Icon24CancelCircleOutline />
+          </Tappable>} />
+        <Textarea
+          header="Описание заметки (необязательно)"
+          placeholder="Например, ссылка на видео или описание, почему вам хотелось бы в будущем вернуться к этому фильму."
+          value={textNoteDescription}
+          onChange={e => setTextNoteDescription(e.target.value)}
+        />
+        <ButtonCell
+          before={<Icon28Done />}
+          onClick={handleSaveTextNote}
+          className={styles.modalCell}
+        >
+          Сохранить
+        </ButtonCell>
+        
+      </Modal>
+    </>
   )
 });
